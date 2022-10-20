a8740e22f6fc566d33dfb271ad7a100d09e0f967
jgelbwaser <99264301+jgelbwaser@users.noreply.github.com>
2022-10-20 19:36:55 +0300

    M2 WIP Add graph funcionality

--- js/CompanyInfo.js
+++ js/CompanyInfo.js
@@ -5,13 +5,13 @@
 class CompanyInfo {
 
     static apiInfoUrl ='https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/';
-    static apiChartUrl = 'https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/historical-price-full/GT';
+    static apiChartUrl = 'https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/historical-price-full/';
     static apiChartQueryString = '?serietype=line';
+    static graphPointMaxQty = 100;
 
     constructor(container, symbol) {
         this.symbol = symbol;
         console.log('container',container);
-        console.log('symbol', symbol);
     }
 
     intcompanyInfoHTML() {
@@ -25,7 +25,8 @@ class CompanyInfo {
     }
 
     async load(){
-        this.companyInfo = await this.getCompanyInfo(this.symbol);
+        const url = CompanyInfo.apiInfoUrl + this.symbol;
+        this.companyInfo = await this.makeApiRequest(url);
         console.log('company info', this.companyInfo);
         // this.image = companyInfo.image;
         // this.name = companyInfo.companyName;
@@ -37,9 +38,56 @@ class CompanyInfo {
         
     }
 
-    async getCompanyInfo(symbol) {
-        const url = CompanyInfo.apiInfoUrl + symbol;
+ 
+    async addChart(){
+        const url = CompanyInfo.apiChartUrl + this.symbol + CompanyInfo.apiChartQueryString;
+        const historicDataArray = await this.makeApiRequest(url);
+        const chartData = this.getChartData(historicDataArray);
+
+
         
+
+        const config = {
+            type: 'line',
+            data: {
+              //labels: dates,
+              datasets: [{
+                label: 'Stock Price History',
+                fill: 'origin', 
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',  
+                pointRadius: 0,
+                pointHoverRadius: 15,     
+                //data: prices
+                data: chartData
+              }]
+            },
+            options: {}
+          };
+      
+      
+          const myChart = new Chart(
+            document.getElementById('companyChart'),
+            config
+          );
+
+    }
+
+    getChartData(historicDataArray) {
+        let data = historicDataArray.historical.map((item) => {
+            return {'x': item.date, 'y': item.close};
+        });
+        if(data.length > 1 && data.length > CompanyInfo.graphPointMaxQty) {
+            const filterDivNum = Math.ceil(data.length/CompanyInfo.graphPointMaxQty);
+            data = data.filter((element, index) => index%filterDivNum == 0);
+        }
+        return data;
+    }
+
+
+   
+
+    async makeApiRequest(url) {
         try {
             const response = await fetch(url);
             if(!response.ok) throw new Error('response status:' + response.status);
@@ -48,12 +96,10 @@ class CompanyInfo {
 
         } catch(err) {
             console.log(err);
-
         }
 
     }
 
-    addChart(){
-        console.log('addChart called');
-    }
+
+ 
 }
